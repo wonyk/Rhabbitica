868bd4669f8b9c279c2643769451f2a853f315a0
Winston Yeo <winstonyeo99@yahoo.com>
2020-01-18 13:34:35 +0800

    Add base telegram bot format and env

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import requests
+import re
+
+
+def get_url():
+    contents = requests.get("https://random.dog/woof.json").json()
+    url = contents["url"]
+    return url
+
+
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+
 def main():
-    pass
+    updater = Updater("979496095:AAHxzNjfJe2qy0piZ1aLUOTdM_MRPLuJkHo")
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("bop", bop))
+    updater.start_polling()
+    updater.idle()
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
