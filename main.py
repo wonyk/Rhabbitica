7f29039b3529dd83bb2b838a70beb346422dacc3
wonyk <yingkeatwon@gmail.com>
2020-01-18 23:20:53 +0800

    Rolback API keys

--- main.py
+++ main.py
@@ -212,8 +212,8 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    # updater = Updater("845289799:AAGynfA8Y3WmzK0oTDFMM92z6ADM04pVyIc", use_context=True)
-    updater = Updater("916014708:AAGdXdRaG-tlpzpiCH05KVk0oO26T6fGVNc", use_context=True)
+    updater = Updater("845289799:AAGynfA8Y3WmzK0oTDFMM92z6ADM04pVyIc", use_context=True)
+    # updater = Updater("916014708:AAGdXdRaG-tlpzpiCH05KVk0oO26T6fGVNc", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
