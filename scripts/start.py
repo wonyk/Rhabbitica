aeffd6afece6e671bf435ff7bba9da8743513c3a
wonyk <yingkeatwon@gmail.com>
2020-01-31 11:39:36 +0800

    A new start dialogue

--- scripts/start.py
+++ scripts/start.py
@@ -7,18 +7,13 @@ _start_sticker = "CAADBQADLwADbc38AdU1wUDmBM3jFgQ"
 
 
 def start(update, context):
-    context.bot.send_sticker(chat_id=update.effective_chat.id, sticker=_start_sticker)
+    context.bot.send_sticker(chat_id=update.message.chat_id, sticker=_start_sticker)
     context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Hello! My name is *Rhabbit*. Welcome to Rhabbitica, an alternate universe of the *Habitica* world! Before we get started, could I trouble you to identify yourself?\n\n*Get your userID.*\nTo find your User ID:\n\t\tFrom the website: User Icon > Settings > API.\n\t\tFrom iOS/Android App: Menu > Settings > API > User ID (tap on it to copy it to your clipboard).\n\n"
-        + "*Get your token Id* \n\t\tTo find your API Token,\nFrom the website: User Icon > Settings > API \n\t\tFrom iOS/Android App: Menu > API > API Token (tap on it to copy it to your clipboard).\n\n"
-        + "*set userID and tokenId after with* /userid _userid here_ *and* /tokenid _tokenid here_ *respectively*",
-        parse_mode="Markdown",
+        chat_id=update.message.chat_id,
+        text="Hello! My name is *Rhabbit*. Welcome to Rhabbitica, an alternate universe of the *Habitica* world! Before we get started, could I trouble you to identify yourself? Please key in your Habitica User ID"
+        + "\n\n*Get your userID.*\nTo find your User ID:\n\t\tFrom the website: User Icon > Settings > API.\n\t\tFrom iOS/Android App: Menu > Settings > API > User ID (tap on it to copy it to your clipboard).\n\n",
+        parse_mode="Markdown"
     )
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text="To get started: /help"
-    )
-
     return SET_USER_ID
     # schedule.every(_quote_time).seconds.do(quote_gen, update, context)
     # schedule.every(_quote_pic_time).seconds.do(quote_pic_gen, update, context)
@@ -30,7 +25,11 @@ def start(update, context):
 
 def get_user_id(update, context):
     uid = update.message.text
-    context.bot.send_message(chat_id=update.message.chat_id, text="User Id set: " + uid)
+    context.bot.send_message(chat_id=update.message.chat_id, text="User Id set: *" + uid + "*.\n\nPlease key in your Habitica User API key."
+    + "\n\n*Get your API key* \n\t\tTo find your API key,\nFrom the website: User Icon > Settings > API \n\t\tFrom iOS/Android App: Menu > API > API Token (tap on it to copy it to your clipboard).\n\n"
+    +"To restart: /restart",
+    parse_mode="Markdown"
+    )
     api.set_user_id(uid)
     return SET_USER_KEY
 
@@ -38,7 +37,10 @@ def get_user_id(update, context):
 def get_user_key(update, context):
     key = update.message.text
     context.bot.send_message(
-        chat_id=update.message.chat_id, text="Token Id set: " + key
+        chat_id=update.message.chat_id, text="API Key set: *" + key + "*.\n\n*Thank you! You may start enjoying Rhabbitica's functions now!*"
+        + "\n\nTo get started: /help"
+        +"\nTo reset your credentials: /start",
+        parse_mode="Markdown"
     )
     api.set_user_key(key)
     return -1
@@ -46,16 +48,16 @@ def get_user_key(update, context):
 
 def restart(update, context):
     logging.info("Unexpected command within conversation")
-    return ConversationHandler.END
 
 
 # Add conversation for init basic data using start
 start_handler = ConversationHandler(
-    entry_points=[CommandHandler("start", start)],
+    entry_points=[CommandHandler("start", start), CommandHandler("restart", start)],
     states={
         SET_USER_ID: [MessageHandler(Filters.text, get_user_id)],
         SET_USER_KEY: [MessageHandler(Filters.text, get_user_key)],
     },
     fallbacks=[CommandHandler("restart", restart)],
+    allow_reentry=True
 )
 
